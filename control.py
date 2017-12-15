d147e8acf17e6c40e006ccbae34bde5095abe135
Paul Kappler <paul.kappler@gmail.com>
2017-12-14 23:09:07 -0800

    outside scene

--- control.py
+++ control.py
@@ -105,7 +105,8 @@ def downstairs_dim():
 def outside_on():
     logger = logging.getLogger('HomeGUI')
     logger.info( "outside_on")
-    b.set_group(10,"on",True) #Outside Front
+    #b.set_group(10,"on",True) #Outside Front
+    b.run_scene("Outside Front", "OutsideBrightest")
 
     refreshList()
 
